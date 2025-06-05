696abace560edbf71fd8d3bf74807191eb7c4dab
wangkor <charwangkor@gmail.com>
2025-06-05 06:02:53 -0700

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+
+import { Button } from '@chakra-ui/react'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+    <Button colorPalette='blue'>
+      click me
+    </Button>
+      
+    </>
+  )
+}
+
+export default App
